155b5a6ed2fed8b00396bc2c9ca71714f67ac2e0
amatgal <amatgal1807@ieszaidinvergeles.org>
2025-09-30 14:30:01 +0200

    Ejercicio 10

---
+++ Ejercicio 10.py
@@ -0,0 +1,16 @@
+'''Una juguetería tiene mucho éxito en dos de sus productos:
+payasos y muñecas. Suele hacer venta por correo y la
+empresa de logística les cobra por peso de cada paquete así
+que deben calcular el peso de los payasos y muñecas que
+saldrán en cada paquete a demanda. Cada payaso pesa 112 g
+y cada muñeca 75 g. Escribir un programa que lea el número
+de payasos y muñecas vendidos en el último pedido y calcule
+el peso total del paquete que será enviado.'''
+
+num_payasos = int(input("Ingrese el número de payasos vendidos: "))
+num_munecas = int(input("Ingrese el número de muñecas vendidas: "))
+peso_payaso = 112  
+peso_muneca = 75  
+peso_total = (num_payasos * peso_payaso) + (num_munecas * peso_muneca)
+print(f"El peso total del paquete es: {peso_total} gramos")    
+ 
