e3b39d98c2de3194d30b7d1cb25dd6659619faf4
amatgal <amatgal1807@ieszaidinvergeles.org>
2025-10-07 13:53:22 +0200

    Ejercicio 2

---
+++ 2 Ejercicios de cadena de caracteres/Ejercicio 5.py
@@ -0,0 +1,6 @@
+'''Escribir un programa que pida al usuario que introduzca una frase en la consola y
+muestre por pantalla la frase invertida.'''
+
+frase = input("Escriba una frase: ")
+frase_invertida = frase[::-1]
+print("La frase invertida es: ", frase_invertida)       
