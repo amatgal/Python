1ea32d46c71723ec9ce34aa9736bdf9ef7161599
amatgal <amatgal1807@ieszaidinvergeles.org>
2025-10-14 13:38:50 +0200

    Ejercicio 3

---
+++ 3 Ejercicios de condicionales/Ejercicio 3.py
@@ -0,0 +1,10 @@
+'''Escribir un programa que pida al usuario dos números y muestre por pantalla su
+división. Si el divisor es cero el programa debe mostrar un error.'''
+
+numero1 = int(input("Introduce un numero que va a actuar como dividendo: "))
+numero2 = int(input("Introduce otro numero que va a actuar como divisor: "))
+
+print ("La division es: ", numero1/numero2)
+
+if numero2 == 0:
+    print ("Error")
