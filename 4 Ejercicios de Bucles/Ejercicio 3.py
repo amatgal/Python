05573569f363af6b33ac97be63d02be00a32beb4
amatgal <amatgal1807@ieszaidinvergeles.org>
2025-11-04 14:13:05 +0100

    Ejercicio 3

--- 4 Ejercicios de Bucles/Ejercicio 3.py
+++ 4 Ejercicios de Bucles/Ejercicio 3.py
@@ -3,6 +3,6 @@ pantalla todos los números impares desde 1 hasta ese número separados por
 comas.'''
 
 numero = int(input("Ingrese un numero entrero positivo: "))
-for i in range(1,numero + 1):
+for i in range(1,numero + 1,2):
     if i % 2 != 0:
         print(str(i), end=",")
