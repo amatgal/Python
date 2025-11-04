3063789945163fe8573727b1998dbc561ec8bd2a
amatgal <amatgal1807@ieszaidinvergeles.org>
2025-11-04 13:14:23 +0100

    Ejercicio 3

---
+++ 4 Ejercicios de Bucles/Ejercicio 3.py
@@ -0,0 +1,8 @@
+'''Escribir un programa que pida al usuario un número entero positivo y muestre por
+pantalla todos los números impares desde 1 hasta ese número separados por
+comas.'''
+
+numero = int(input("Ingrese un numero entrero positivo: "))
+for i in range(1,numero + 1):
+    if i % 2 != 0:
+        print(str(i), end=",")
