a8832a634258e2b2e5266f6cfc9f0a867d52e5bd
amatgal <amatgal1807@ieszaidinvergeles.org>
2025-12-03 10:13:22 +0100

    Ejercicio 13

---
+++ 5 Ejercicios de Listas y Tuplas/Ejercicio 13.py
@@ -0,0 +1,23 @@
+'''Escribir un programa que almacene las matrices
+en una lista y muestre por pantalla su producto.
+Nota: Para representar matrices mediante listas usar listas anidadas, representando
+cada vector fila en una lista.'''
+#Si las declaro entre parentesis es como si fuera una tupla (no se pueden modificar), 
+# y si lo hago entre corchetes es una lista (se pueden modificar).
+
+A = ((1, 2, 3),
+     (4, 5, 6))     
+B = ((-1, 0),
+     (0, 1),
+     (1, 1)) 
+resultado = [[0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 0]] 
+
+for i in range(len(A)):          
+    for j in range (len(B[0])):      
+        for k in range(len(B)):  
+            resultado[i][j] += A[i][k] * B[k][j]
+for r in resultado:
+    print(r)
+    
