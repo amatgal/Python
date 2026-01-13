d2c5c4832f5a25a53100ad6d8e3efe07e91c8d4e
amatgal <amatgal1807@ieszaidinvergeles.org>
2026-01-13 14:15:22 +0100

    Ejercicio 11

--- 6 Ejercicios de Diccionarios/Ejercicio 11.py
+++ 6 Ejercicios de Diccionarios/Ejercicio 11.py
@@ -26,3 +26,20 @@ decir, un diccionario como el siguiente
 '98376547F': {'nombre': 'Carmen Sánchez', 'email':
 'carmen@mail.com', 'teléfono': '667677855', 'descuento': 15.7}}'''
 
+directorio = "nif;nombre;email;teléfono;descuento\n01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7"
+DirectorioD = {}
+
+lista_clientes = directorio.split('\n')
+lista_campos = lista_clientes[0].split(';')
+
+for i in lista_clientes[1:]:
+    diccionario_cliente = {}
+    lista_cliente = i.split(';')
+    for j in range(len(lista_cliente)):
+        if j == 0:
+            nif = lista_cliente[j]
+        else:
+            diccionario_cliente[lista_campos[j]] = lista_cliente[j]
+        DirectorioD[nif] = diccionario_cliente
+print(DirectorioD)
+
