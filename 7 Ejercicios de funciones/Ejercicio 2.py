c3352c54a1e549f85c43d5a1e196f1c216c996a0
amatgal <amatgal1807@ieszaidinvergeles.org>
2026-01-20 13:24:41 +0100

    Ejercicio 2

---
+++ 7 Ejercicios de funciones/Ejercicio 2.py
@@ -0,0 +1,6 @@
+'''Escribir una función a la que se le pase una cadena <nombre> y muestre por
+pantalla el saludo ¡hola <nombre>!.'''
+
+def saludar(nombre):
+    print(f"¡Hola {nombre}!")   
+saludar("Ana")
---
+++ 7 Ejercicios de funciones/Ejercicio 3.py
@@ -0,0 +1,13 @@
+'''Escribir una función que reciba un número entero positivo y devuelva su factorial.'''
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+num = int(input("Introduce un número entero positivo: "))
+if num < 0:
+    print("El número debe ser positivo.")
+else:
+    print(f"El factorial de {num} es {factorial(num)}") 
+    
